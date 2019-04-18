8139ef9128c67af003b751be4b193ee75aeaafcd
LucLev <luckylevillain@gmail.com>
2019-04-18 18:41:47 +0200

    Premier jet du systeme de combat

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Player } from '../player';
+import { Enemy } from '../enemy';
+import { Egg } from '../egg';
 
 @Component({
   selector: 'app-game',
@@ -11,5 +14,30 @@ export class GameComponent implements OnInit {
 
   ngOnInit() {
   }
+  eggInventory = [];
+  private delay(ms: number)
+    {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+  private async fight(){
+    let currentEnemy: Enemy
+    let enemyEgg: Egg
+    while(Player.health > 0 && currentEnemy.health > 0){
+      Enemy.health -= (Player.attack - currentEnemy.defense);
+      // 1 sec
+      await this.delay(1000);
 
+      Player.health -= (currentEnemy.attack - Player.defense);
+      // 1 sec
+      await this.delay(1000);
+
+    }
+    if(Player.health <= 0 ) {
+      alert("You died")
+    }
+    else{
+      this.eggInventory.push(enemyEgg)
+    }
+  }
+*/
 }
