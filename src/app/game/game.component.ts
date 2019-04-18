3f8c4790f8c73d07ed941808b19a88a8544eaca9
LucLev <luckylevillain@gmail.com>
2019-04-18 23:01:09 +0200

    merde

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -20,32 +20,36 @@ export class GameComponent implements OnInit {
 
   ngOnInit() {
 
-    this.enemyService.getRandomEnemy().subscribe(
-      (param_data:Enemy) => {
-        this.data = param_data;
-      }
-    )
+
   }
   eggInventory = [];
+  inFight: boolean = true;
   player: Player = new Player;
+  currentEnemy: Enemy = new Enemy;
   private delay(ms: number)
     {
       return new Promise(resolve => setTimeout(resolve, ms));
     }
   private async fight(){
-    let currentEnemy: Enemy = new Enemy;
+    this.enemyService.getRandomEnemy().subscribe(
+      (param_data:Enemy) => {
+        this.data = param_data;
+      }
+    )
+    //let currentEnemy: Enemy = new Enemy;
     let enemyEgg: Egg = new Egg;
-    while(this.player.health > 0 && currentEnemy.health > 0){
-      currentEnemy.health -= (this.player.attack - currentEnemy.defense);
-      // 1 sec
-      await this.delay(1000);
-      console.log(currentEnemy.health)
-      if(currentEnemy.health <= 0){
+    this.inFight = false;
+    while(this.player.health > 0 && this.currentEnemy.health > 0){
+      this.currentEnemy.health -= (this.player.attack - this.currentEnemy.defense);
+      // 0.5 sec
+      await this.delay(500);
+      console.log(this.currentEnemy.health)
+      if(this.currentEnemy.health <= 0){
         break
       }
-      this.player.health -= (currentEnemy.attack - this.player.defense);
-      // 1 sec
-      await this.delay(1000);
+      this.player.health -= (this.currentEnemy.attack - this.player.defense);
+      // 0.5 sec
+      await this.delay(500);
       console.log(this.player.health)
     }
     if(this.player.health <= 0 ) {
@@ -54,6 +58,8 @@ export class GameComponent implements OnInit {
     else{
       this.eggInventory.push(enemyEgg)
       alert("you won an egg")
+      this.inFight = true;
     }
   }
 }
+
