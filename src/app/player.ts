382ff607f5c8c7ab8ebcf01cb6e84ca7e5f7f926
LucLev <luckylevillain@gmail.com>
2019-04-19 16:12:38 +0200

    last commit of hackathon

--- src/app/player.ts
+++ src/app/player.ts
@@ -1,7 +1,7 @@
 export class Player {
     name:string;
-    health:number = 100;
-    maxHealth:number = 100;
+    health:number = 150;
+    maxHealth:number = 150;
     attack:number = 10;
     defense:number = 0;
 }
